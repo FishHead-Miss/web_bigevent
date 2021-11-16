0139fa6184d9e474e66d61dc89b5dc6ccbecaf4b
yutong <1181493906@qq.com>
2021-11-16 20:02:22 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,36 +13,37 @@ $(function() {
             });
 
         });
-    })
-    // 获取用户的基本信息
-function getUserInfo() {
-    $.ajax({
-        method: "GET",
-        url: "/my/userinfo",
-        // 从本地获取请求头的权限
-        // headers: { Authorization: localStorage.getItem("token") || "" },
-        success: function(res) {
-            if (res.status !== 0) {
-                return layui.layer.msg("获取用户信息失败!");
+
+        function getUserInfo() {
+            $.ajax({
+                method: "GET",
+                url: "/my/userinfo",
+                // 从本地获取请求头的权限
+                // headers: { Authorization: localStorage.getItem("token") || "" },
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layui.layer.msg("获取用户信息失败!");
+                    }
+                    renderAvatar(res.data);
+                }
+            })
+        }
+
+        //渲染用户头像
+        function renderAvatar(user) {
+            var name = user.nickname || user.username;
+            $("#welcome").html("欢迎&nbsp;&nbsp" + name);
+
+            // 按需渲染用户头像
+            if (user.user_pic !== null) {
+                $(".layui-nav-img").attr("src", user.user_pic).show();
+                $(".text-avatar").hide();
+
+            } else {
+                $(".layui-nav-img").hide();
+                var first = name[0].toUpperCase();
+                $(".text-avatar").html(first).show();
             }
-            renderAvatar(res.data);
         }
     })
-}
-
-//渲染用户头像
-function renderAvatar(user) {
-    var name = user.nickname || user.username;
-    $("#welcome").html("欢迎&nbsp;&nbsp" + name);
-
-    // 按需渲染用户头像
-    if (user.user_pic !== null) {
-        $(".layui-nav-img").attr("src", user.user_pic).show();
-        $(".text-avatar").hide();
-
-    } else {
-        $(".layui-nav-img").hide();
-        var first = name[0].toUpperCase();
-        $(".text-avatar").html(first).show();
-    }
-}
+    // 获取用户的基本信息
